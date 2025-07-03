9f89a12390c4ba33c02b2bdd761bd4b0f38c78b3
mrtn <martinakopyan34@gmail.com>
2025-07-03 18:44:09 +0400

    Add primeVue.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss', "@primevue/nuxt-module"],
 	css: [
     '@/assets/scss/main.scss' // path to your main SCSS file
   ],
+	primevue: {
+		autoImport: true,
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: "p",
+					darkModeSelector: false
+        },
+      },
+    },
+  },
 })
